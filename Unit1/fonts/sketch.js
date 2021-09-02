cd02f8c0747c349813ecf7897f6bd98e36871a6c
Kuhn2001 <clkuhn1@ilstu.edu>
2021-09-02 10:09:17 -0500

    fonts good

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,14 +1,20 @@
-var font1;
+let f1,f2;
 
 
 function setup() {
   createCanvas(500, 500);
-  font1 = loadFont("assets/Architects_Daughter/Daughter.ttf");
+  f1 = loadFont("assets/Architects_Daughter/Daughter.ttf");
+  f2 = loadFont("assets/Rampart_One/RampartOne.ttf");
 }
 
 function draw() {
   background("yellow");
-textFont(font1);
+textFont(f1);
 textSize(24);
-text("hello world",100,100);
+text("hello world",width/2,100);
+
+
+textFont(f2);
+textSize(24);
+text("Sic Parvis Magna", width/2,160);
 }
