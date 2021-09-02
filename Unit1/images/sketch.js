9386ecf723e00d188db038ecf81d040c8eddd3f1
Kuhn2001 <clkuhn1@ilstu.edu>
2021-09-02 09:59:03 -0500

    right

--- Unit1/images/sketch.js
+++ Unit1/images/sketch.js
@@ -1,15 +1,16 @@
-let adventure, bmx, fruit, germany, ledzeppelin;
+let adventure, germany, ledzeppelin;
 
 function setup() {
   createCanvas(500, 500);
   adventure = loadImage("assets/adventure.jpg")
   germany = loadImage("assets/germany.jpg")
   ledzeppelin = loadImage("assets/ledzeppelin.jpg")
+  imageMode(CENTER);
 }
 
 function draw() {
   background('orange');
-image(adventure, width/2,height/2,100,-120,100,100);
-image(germany, width/2,height/2,-120,100,100);
-image(ledzeppelin, width/2,height/2,+120,100,100);
+image(adventure, width/2,height/2,100,100);
+image(germany, width/2,height/2-120,100,100);
+image(ledzeppelin, width/2,height/2+120,100,100);
 }
