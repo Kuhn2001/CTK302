baf0f30d393b608710d3930dc628865cef9d042c
Kuhn2001 <clkuhn1@ilstu.edu>
2021-09-16 14:06:40 -0500

    crazy

--- Unit2/state/sketch.js
+++ Unit2/state/sketch.js
@@ -1,7 +1,72 @@
+let state = 0;
+let timer = 0;
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(400, 400);
 }
 
 function draw() {
+  background(220);
 
+  switch(state){
+
+    case 0:
+      background('red');
+      for (var j = 0 ; j <= height ; j+=20) {
+      for(var i = 0 ; i <= width ; i+=20){
+        fill(random(54),random(255),random(134));
+        stroke(13, 247, 255)
+    rect(i,j,5,5) ;
+    }
+  }
+
+      break;
+      case 1:
+      background(255, 73, 13);
+      for (var j = 0 ; j <= height ; j+=20) {
+      for(var i = 0 ; i <= width ; i+=20){
+        fill(random(255),random(88),random(166));
+        noStroke();
+    rect(i,j,5,15) ;
+    }
+  }
+
+      break;
+
+       case 2:
+      background(18, 204, 255);
+      for (var j = 0 ; j <= height ; j+=20) {
+      for(var i = 0 ; i <= width ; i+=20){
+      fill(random(255),random(0),random(105));
+      ellipse(i,j,30,30) ;
+}
+}
+      break;
+
+       case 3:
+      background(random(13),random(186),random(255));
+      for (var j = 0 ; j <= height ; j+=10) {
+      for(var i = 0 ; i <= width ; i+=10){
+    fill(247, 255, 13);
+    arc(i, j, 10, 10, PI, TWO_PI);
+  }
+}
+      break;
+
+       case 4:
+      background(0);
+      for (var j = 0 ; j <= height ; j+=20) {
+      for(var i = 0 ; i <= width ; i+=20){
+        fill(random(255));
+        rect(i, j, 5, 20);
+  }
+}
+      break;
+  }
+}
+function mouseReleased(){
+  state++;
+  if(state > 4){
+    state =0;
+  }
 }
--- Unit2/timer/sketch.js
+++ Unit2/timer/sketch.js
@@ -1,7 +1,48 @@
+let state = 0;
+let timer = 0;
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(400, 400);
 }
 
 function draw() {
+  background(220);
 
+  switch(state){
+
+    case 0:
+      background('red');
+      //text("state 0", 100, 100);
+      for (var j = 0 ; j <= height ; j+=20) {
+      for(var i = 0 ; i <= width ; i+=20){
+    rect(i,j,5,5) ;
+    }
+  }
+
+      break;
+      case 1:
+      background('yellow');
+      //text("state 0", 100, 100);
+
+      for (var j = 0 ; j <= height ; j+=20) {
+      for(var i = 0 ; i <= width ; i+=20){
+        fill(random(255));
+    rect(i,j,5,15) ;
+    }
+  }
+
+      break;
+ }
+ timer++;
+if (timer > 5 * 60){
+  timer = 0;
+
+  state++;
+  if(state > 4){
+    state = 0;
+  }
+
+
+
+}
 }
