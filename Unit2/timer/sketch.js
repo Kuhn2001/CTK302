37017b4b29abf40f0e15123284a59b260764dc1a
Kuhn2001 <clkuhn1@ilstu.edu>
2021-09-21 14:25:38 -0500

    poem

--- Unit2/timer/sketch.js
+++ Unit2/timer/sketch.js
@@ -1,8 +1,10 @@
 let state = 0;
 let timer = 0;
+let f1;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
+    f1 = loadFont("assets/LoversQuarrel.ttf");
 }
 
 function draw() {
@@ -11,38 +13,36 @@ function draw() {
   switch(state){
 
     case 0:
-      background('red');
-      text("You don't need a parachute to go skydiving", 100, 100);
-      for (var j = 0 ; j <= height ; j+=20) {
-      for(var i = 0 ; i <= width ; i+=20){
-    rect(i,j,5,5) ;
-    }
-  }
-
+      background(252, 96, 145);
+      fill(209, 0, 21);
+      textSize(40);
+      textFont(f1);
+      text("Roses are Red", 100, 100);
       break;
+
+
       case 1:
-      background('yellow');
-      text("you need a parachute to go skydiving twice.", 100, 100);
-
-      for (var j = 0 ; j <= height ; j+=20) {
-      for(var i = 0 ; i <= width ; i+=20){
-        fill(random(255));
-    rect(i,j,5,15) ;
-    }
-  }
-
+      background(65, 185, 250);
+      fill(163, 56, 245);
+      textSize(40);
+      textFont(f1);
+      text("Violets are Blue", 150, 150);
       break;
+
+      case 2:
+      background(247, 240, 15);
+      fill(240, 151, 43);
+      textSize(50);
+      textFont(f1);
+      text("I forgot the Word and\n so have you", 200, 200);
+      break;
+
  }
  timer++;
-if (timer > 5 * 60){
-  timer = 0;
-
-  state++;
-  if(state > 4){
-    state = 0;
+ if (timer > 3 * 60) {
+   timer = 0;
+   state++;
+  if (state > 2) state = 0;
   }
 
-
-
-}
 }
